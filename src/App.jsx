31954aeaac99ad3061ada6a635a930103aa3f82a
AFESAID <123980177+AFESAID@users.noreply.github.com>
2023-07-25 18:56:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Img from "./components/input"
+
+function App() {
+
+  return (
+    <>
+     <img src='../public/ADSOimg2.jpg'/>
+     <br />
+     <img src='../public/SENAimg.jpg'/>
+     <br />
+     <textarea name="Nombre" id="Nombre" cols="50" rows="10" placeholder="Descripcion"></textarea>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './input.css'
+
+function Img({img}){
+    return(
+        <div>
+            <img img={img}/>
+        </div>
+    )
+}
+
+export default Img
